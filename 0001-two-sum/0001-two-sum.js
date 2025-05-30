c3d92901b3863d6c39117f2f64cbe9cd819ed238
biyash <sthaalish39@gmail.com>
2025-05-30 23:45:02 +0545

    Time: 2 ms (70.67%), Space: 54.8 MB (52.17%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,18 @@
+function twoSum(nums, target) {
+      const map = {}; 
+
+        for (let i = 0; i < nums.length; i++) {
+            const num = nums[i];
+                const complement = target - num;
+
+                    if (map.hasOwnProperty(complement)) {
+                          return [map[complement], i];
+                              }
+
+                                  map[num] = i;
+                                    }
+
+                                   
+                                        return [];
+                                        }
+
